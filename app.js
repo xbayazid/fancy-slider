ad02c2a5d555aa5c00ad952fb846f0638e65dd77
xbayazid <bayazid9006@gmail.com>
2022-09-16 12:34:08 +0600

    completed

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const showImages = (images) => {
 const getImages = (query) => {
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hitS))
+    .then(data => showImages(data.hits))
     .catch(err => console.log(err))
 }
 
@@ -67,7 +67,7 @@ const createSlider = () => {
   document.querySelector('.main').style.display = 'block';
   // hide image aria
   imagesArea.style.display = 'none';
-  const duration = document.getElementById('duration').value || 1000;
+  const duration =  1000;
   sliders.forEach(slide => {
     let item = document.createElement('div')
     item.className = "slider-item";
